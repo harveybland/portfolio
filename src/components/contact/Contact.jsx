e3048bb19dae786a5ce7ad95aa277e0c08249acc
harveybland <harvey.bland@genius.online>
2022-06-27 10:10:53 +0100

    ternary operator

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -49,17 +49,26 @@ const Contact = () => {
           </div>
         </div>
         <div className='c-right'>
-          <p className='c-desc'>
-            <strong>What's your story?</strong> Get in touch.
-          </p>
-          <form ref={formRef} onSubmit={handleSubmit}>
-            <input type='text' placeholder='Name' name='user_name' />
-            <input type='text' placeholder='Subject' name='user_subject' />
-            <input type='text' placeholder='Email' name='user_email' />
-            <textarea name='message' placeholder='Message' rows='5'></textarea>
-            <button>Submit</button>
-            {done && 'Thank you...'}
-          </form>
+          {done ? (
+            'Thank you, we will be in touch.'
+          ) : (
+            <div>
+              <p className='c-desc'>
+                <strong>What's your story?</strong> Get in touch.
+              </p>
+              <form ref={formRef} onSubmit={handleSubmit}>
+                <input type='text' placeholder='Name' name='user_name' />
+                <input type='text' placeholder='Subject' name='user_subject' />
+                <input type='text' placeholder='Email' name='user_email' />
+                <textarea
+                  name='message'
+                  placeholder='Message'
+                  rows='5'
+                ></textarea>
+                <button>Submit</button>
+              </form>
+            </div>
+          )}
         </div>
       </div>
     </div>
